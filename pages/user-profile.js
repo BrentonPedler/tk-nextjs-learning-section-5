daa4dd69683fc1fe6bfecf2bf9455f7e65376434
BrentonPedler <bpedler@trewknowledge.com>
2023-09-19 14:28:40 -0400

    Tuesday Training Update

---
+++ pages/user-profile.js
@@ -0,0 +1,17 @@
+function userProfilePage(props) {
+	return (
+		<h1>{props.username}</h1>
+	)
+}
+
+export default userProfilePage;
+
+export async function getServerSideProps(context) {
+	const { params, req, res } = context;
+
+	return {
+		props: {
+			username: 'Brenton'
+		}
+	}
+}
